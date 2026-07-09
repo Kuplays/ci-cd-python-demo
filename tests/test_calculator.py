4a0ced2e288804550e9c79ba6e985492ff61bb7e
kuplays <alexkuplays@gmail.com>
2026-07-09 20:10:55 +0300

    Added allure results generation job

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,38 +1,68 @@
+import allure
 import pytest
 
 from app.calculator import add, divide, multiply, power, subtract
 
 
+@allure.feature("Calculator")
+@allure.story("Addition")
+@allure.title("Add two numbers")
 def test_add_two_positive_numbers():
-    result = add(2, 3)
+    with allure.step("Calculate 5 + 5"):
+        result = add(5, 5)
 
-    assert result == 5
+    with allure.step("Check result is 10"):
+        assert result == 10
 
 
+@allure.feature("Calculator")
+@allure.story("Subtraction")
+@allure.title("Subtract two numbers")
 def test_subtract_two_numbers():
-    result = subtract(10, 4)
+    with allure.step("Calculate 5 - 5"):
+        result = subtract(5, 5)
 
-    assert result == 6
+    with allure.step("Check result is 0"):
+        assert result == 0
 
 
+@allure.feature("Calculator")
+@allure.story("Multiplication")
+@allure.title("Multiply two numbers")
 def test_multiply_two_numbers():
-    result = multiply(3, 4)
+    with allure.step("Calculate 5 * 5"):
+        result = multiply(5, 5)
 
-    assert result == 12
+    with allure.step("Check result is 25"):
+        assert result == 25
 
 
+@allure.feature("Calculator")
+@allure.story("Division")
+@allure.title("Divide two numbers")
 def test_divide_two_numbers():
-    result = divide(10, 2)
+    with allure.step("Calculate 5 / 5"):
+        result = divide(5, 5)
 
-    assert result == 5.0
+    with allure.step("Check result is 1.0"):
+        assert result == 1.0
 
 
+@allure.feature("Calculator")
+@allure.story("Division")
+@allure.title("Divide by zero raise an error")
 def test_divide_by_zero_raises_error():
-    with pytest.raises(ValueError, match="Division by zero is not allowed"):
-        divide(10, 0)
+    with allure.step("Try to divide by 0"):
+        with pytest.raises(ValueError, match="Division by zero is not allowed"):
+            divide(5, 0)
 
 
+@allure.feature("Calculator")
+@allure.story("Power")
+@allure.title("Raise number to a power")
 def test_power():
-    result = power(2, 3)
+    with allure.step("Calculate 5^2"):
+        result = power(5, 2)
 
-    assert result == 8
+    with allure.step("Check result is 25"):
+        assert result == 25
